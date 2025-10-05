f9ee73b503ec290a01b6d49bba8eef6db8218e71
AkshayKrishnan1999 <52628204+AkshayKrishnan1999@users.noreply.github.com>
2025-10-05 11:58:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/Career-Conversation-chatbot-embedded-in-resume-website/",
   server: {
     host: "::",
     port: 8080,
